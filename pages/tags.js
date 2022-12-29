5013e0b58ac6eb01500b2905201b0a6c7d7fc025
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-12-29 12:47:45 -0300

    tags api

---
+++ pages/tags.js
@@ -0,0 +1,15 @@
+import { dbConnect } from '../../recruit-next/utils/mongoose'
+import Tag from '../../recruit-next/models/tags'
+
+dbConnect()
+
+export default async function handler(req, res) {
+    const { method } = req
+    if (req.method === 'GET'){
+        const tags = await Tag.find();
+        res.status(200).json({method, tags});
+    }
+    if (req.method === 'POST'){
+        res.status(200).json({method, name: "POST Request"});
+    }
+  }
